5646ba01e425278bc745fe051c0557a49ca9338a
Vlad Stepanov <prolda31@gmail.com>
2025-10-28 20:30:16 -0700

    sv

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,5 +14,10 @@ export default clerkMiddleware(async (auth, req) => {
 });
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/api/trpc/:path*"],
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+    "/dashboard/:path*",
+    "/auth-callback",
+  ],
 };
