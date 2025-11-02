f3810b464b0eba2c0c61f6a0ddf0e96866b04085
Vlad Stepanov <prolda31@gmail.com>
2025-11-02 12:15:54 -0800

    sv

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
-const isPublicRoute = createRouteMatcher(["/api/uploadthing(.*)"]);
+const isPublicRoute = createRouteMatcher(["/api/uploadthing(.*)", "/api/webhooks/stripe(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   const url = req.nextUrl.pathname;
