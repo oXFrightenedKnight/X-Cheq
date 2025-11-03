7993164b8522fecdb9a5cf3f5eb0fecf597e490a
Vlad Stepanov <prolda31@gmail.com>
2025-11-02 18:34:12 -0800

    sv

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,12 +1,16 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "@/trpc";
+import { auth } from "@clerk/nextjs/server";
 
-function handler(req: Request) {
+async function handler(req: Request) {
+  const { userId } = await auth();
   return fetchRequestHandler({
     endpoint: "/api/trpc",
     req,
     router: appRouter,
-    createContext: () => ({}),
+    createContext: () => ({
+      userId,
+    }),
   });
 }
 export { handler as GET, handler as POST };
