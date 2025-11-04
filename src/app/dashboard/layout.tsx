0c78c35cacf6ce60a46df2d308582b5e4070d740
Vlad Stepanov <prolda31@gmail.com>
2025-11-03 22:27:51 -0800

    sv

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,5 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-export const dynamic = "force-dynamic";
 
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
   const { userId } = await auth();
